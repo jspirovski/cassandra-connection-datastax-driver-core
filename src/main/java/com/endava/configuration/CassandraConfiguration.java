03b362a1a7e52ee9d7e664771bd2d864b84bf5f0
jspirovski <jovica.spirovski@gmail.com>
2016-12-30 13:29:29 +0100

    Cassandra CRUD

--- src/main/java/com/endava/configuration/CassandraConfiguration.java
+++ src/main/java/com/endava/configuration/CassandraConfiguration.java
@@ -28,14 +28,16 @@ public class CassandraConfiguration {
         Author authorSelect = authorMapper.get(UUID.fromString("efc730c1-8501-4da5-8ab7-858445409191"));
         Date birthDate = authorSelect.getBirth_date();
 
-        Author authorInsert = new Author(UUID.randomUUID(), "beti", "spasovska", DateCassandra.dateStringToDate("2001-08-14 12:44"));
+        Author authorInsert = new Author(UUID.randomUUID(), "john", "smith", DateCassandra.dateStringToDate("2001-08-14 12:44"));
         authorMapper.save(authorInsert);
 
-        Author authorUpdate = new Author(UUID.fromString("a51294df-0d16-4fb3-85e9-2e2ecc7ac1a2"), "jovan", "shalamanoski", DateCassandra.dateStringToDate("1987-12-11 12:44"));
+        Author authorUpdate = new Author(UUID.fromString("a51294df-0d16-4fb3-85e9-2e2ecc7ac1a2"), "joe", "doe", DateCassandra.dateStringToDate("1987-12-11 12:44"));
         authorMapper.save(authorUpdate);
 
 
         authorMapper.delete(UUID.fromString("20529cb7-98d1-4989-bd07-815d8a02a6f0"));
+
+
 
 
 
